43abddb50ba33c99fa810e5d2a2facfea041e80e
Chu-Cheng Lin <chuchenglin@cmu.edu>
2020-11-26 22:23:32 +0800

    typo

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ def train():
         total_loss += loss.item()
         optimizer.param_groups[0]['lr'] = lr2
         if batch % args.log_interval == 0 and batch > 0:
-            cur_loss = total_loss.item() / args.log_interval
+            cur_loss = total_loss / args.log_interval
             elapsed = time.time() - start_time
             logging('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5e} | ms/batch {:5.2f} | '
                     'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
