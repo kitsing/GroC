ab1a685cff714e99a4af31fedfe89aab0df1a3e3
Chu-Cheng Lin <chuchenglin@cmu.edu>
2020-11-27 15:54:47 +0800

    fix

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ class Dictionary(object):
     def add_word(self, word):
         if word not in self.word2idx:
             self.idx2word.append(word)
-            self.word2idx[word] = len(self.idx2word)
+            self.word2idx[word] = len(self.idx2word) - 1
         return self.word2idx[word]
 
     def __len__(self):
